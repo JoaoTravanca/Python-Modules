3b2e408c084de1d0dcdcbde2cce3c2a01a510f53
João <joaotptravanca@gmail.com>
2026-07-20 14:28:48 +0100

    ex00

--- Module2/ex00/ft_first_exception.py
+++ Module2/ex00/ft_first_exception.py
@@ -1,8 +1,22 @@
-def test_value(temp):
-	try:
-	
-	except x_error: 
-	
-def input_temperature(temp_str):
-	print(f"input data is '{int(temp_str)}'")
-	test_value(int(temp_str))
+def input_temperature(temp_str) -> int:
+    return int(temp_str)
+
+
+def test_temperature(temp) -> None:
+    try:
+        print(f"input data is '{temp}'")
+        temp = input_temperature(temp)
+        print(f"Temperature is now {temp}°C")
+    except ValueError:
+        print(f"Caught input_temperature error: invalid literal for int() "
+              f"with base 10: {temp}")
+
+
+if __name__ == "__main__":
+    print("=== Garden Temperature ===")
+    print("")
+    test_temperature(25)
+    print("")
+    test_temperature("abc")
+    print("")
+    print("All tests completed - program didn't crash!")
