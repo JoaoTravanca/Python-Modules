058223ccd54f300c0b4f3e4ba43b2bd7d8a4b85e
João <joaotptravanca@gmail.com>
2026-07-20 15:09:43 +0100

    ex 1 and fix

--- Module2/ex00/ft_first_exception.py
+++ Module2/ex00/ft_first_exception.py
@@ -2,21 +2,25 @@ def input_temperature(temp_str) -> int:
     return int(temp_str)
 
 
-def test_temperature(temp) -> None:
+def test_temperature() -> None:
+    print("=== Garden Temperature ===")
+    input = "25"
     try:
-        print(f"input data is '{temp}'")
-        temp = input_temperature(temp)
+        print(f"input data is {input}")
+        temp = input_temperature(input)
         print(f"Temperature is now {temp}°C")
     except ValueError:
         print(f"Caught input_temperature error: invalid literal for int() "
               f"with base 10: {temp}")
+    input = "abc"
+    try:
+        print(f"input data is '{input}'")
+        temp = input_temperature(input)
+        print(f"Temperature is now {temp}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+    print("All tests completed - program didn't crash!")
 
 
 if __name__ == "__main__":
-    print("=== Garden Temperature ===")
-    print("")
-    test_temperature(25)
-    print("")
-    test_temperature("abc")
-    print("")
-    print("All tests completed - program didn't crash!")
+    test_temperature()
--- Module2/ex01/ft_raise_exception.py
+++ Module2/ex01/ft_raise_exception.py
@@ -1,16 +1,47 @@
 def input_temperature(temp_str) -> int:
-    return int(temp_str)
+    temp = int(temp_str)
+    if (temp < 0 or temp > 40):
+        if (temp < 0):
+            raise ValueError(f"Caught input_temperature error: {temp}"
+                             f" is too cold for plants (min 0°C)")
+        elif (temp > 40):
+            raise ValueError(f"Caught input_temperature error: {temp}"
+                             f" is too hot for plants (max 40°C)")
+    return temp
 
 
-def test_temperature(temp) -> None:
+def test_temperature() -> None:
+    print("=== Garden Temperature ===")
+    value = "25"
     try:
-        print(f"Input data is '{temp}'")
-        temp = input_temperature(temp)
+        print(f"input data is {value}")
+        temp = input_temperature(value)
         print(f"Temperature is now {temp}°C")
-    except ValueError:
-        print(f"Caught input_temperature error: invalid literal for int() "
-              f"with base 10: {temp}")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+    value = "abc"
+    try:
+        print(f"input data is '{value}'")
+        temp = input_temperature(value)
+        print(f"Temperature is now {temp}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+    value = "100"
+    try:
+        print(f"input data is '{value}'")
+        temp = input_temperature(value)
+        print(f"Temperature is now {temp}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+    value = "-50"
+    try:
+        print(f"input data is '{value}'")
+        temp = input_temperature(value)
+        print(f"Temperature is now {temp}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+    print("All tests completed - program didn't crash!")
 
 
 if __name__ == "__main__":
-    print("=== Garden Temperature checker ===")
+    test_temperature()
